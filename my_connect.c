96a857fd3323712ef3fd1e794a4b232c02d0364f
az <absolutezero2a03@gmail.com>
2014-12-15 09:38:29 -0800

    asdfasdf

--- my_connect.c
+++ my_connect.c
@@ -2,7 +2,6 @@
 #include "connect_socks.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <fcntl.h>
 int socket(int domain, int type, int protocol) {
     if (len_proxy_fds == 65536) {
@@ -28,7 +27,7 @@ int socket(int domain, int type, int protocol) {
         */
 
         //printf("getaddrinfo status: %d\n",status);
-        struct addrinfo *socks_info = get_socks_addr("127.0.0.1","9050");
+        struct addrinfo *socks_info = get_socks_addr("localhost","9050");
         int sockfd = get_socks_fd(socks_info);
         //printf("socket called: %d\n",sockfd);
                 
@@ -72,13 +71,13 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
             nonblocking = 0;
         }
 
-        fcntl(sockfd,F_SETFL,(opts & (~O_NONBLOCK))); //might change to | if it doesn't work
-
-        //printf("connect called\n");
-        struct addrinfo *socks_info = get_socks_addr("127.0.0.1","9050");
+        if (nonblocking) {
+            fcntl(sockfd,F_SETFL,(opts ^ O_NONBLOCK)); 
+        }
+        struct addrinfo *socks_info = get_socks_addr("localhost","9050");
         int connected = (*og_connect)(sockfd,socks_info->ai_addr,socks_info->ai_addrlen);
         if (connected!=0) {
-            printf("connect failed: %s\n",strerror(errno));
+            printf("connect failed.\n");
             return -1;
         }
         char buffer[256];
@@ -90,7 +89,7 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
         recv(sockfd,buffer,2,0);
         if (buffer[1] != 0) {
             printf("SOCKS handshake failed\n");
-            return (*og_connect)(sockfd,addr,addrlen);
+            return -1;
         }
         int i;
 
@@ -100,7 +99,7 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
         }
         printf("\n");
         char r_res[256],*req;
-        char req_res[256];
+        char req_res[10];
  
       
         req=r_res;
@@ -114,8 +113,8 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
         req+=sizeof(*e_addr);
 
         unsigned short int sin_port = ipv4->sin_port; 
-        *(req++) = sin_port >> 8;
         *(req++) = sin_port & 0xff;
+        *(req++) = sin_port >> 8;
 
         send(sockfd,r_res,req-r_res,0);
         int k;
@@ -135,7 +134,7 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
             fcntl(sockfd,F_SETFL,(opts | O_NONBLOCK));
         }
         if (req_res[1] != 0) {
-            printf("-1\n");
+            printf("SOCKS not letting you connect.\n");
             return -1;
         } else {
             return 0;
